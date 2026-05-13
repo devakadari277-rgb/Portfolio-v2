7dee44a4e042963ea8dd6cadaf59e07dcbef9fd1
devakadari277-rgb <devakadari277@gmail.com>
2026-05-14 00:13:37 +0530

    removed scroll animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,73 +1,77 @@
-import { BrowserRouter } from "react-router-dom"
-import { Container } from "./styles"
-import ScrollAnimation from "react-animate-on-scroll"
-import Illustration from "../../assets/illustration.svg"
-import { NavHashLink } from "react-router-hash-link"
-import linkedin from '../../assets/linkedin.svg'
-import githubIcon from '../../assets/github.svg'
-import whatsapp from '../../assets/whatsapp.svg'
-import Hello from '../../assets/Hello.gif'
-import telegram from '../../assets/telegram.svg'
+import { BrowserRouter } from "react-router-dom";
+import { Container } from "./styles";
+import Illustration from "../../assets/illustration.svg";
+import { NavHashLink } from "react-router-hash-link";
+
+import linkedin from "../../assets/linkedin.svg";
+import githubIcon from "../../assets/github.svg";
+import whatsapp from "../../assets/whatsapp.svg";
+import Hello from "../../assets/Hello.gif";
+import telegram from "../../assets/telegram.svg";
+
 export function Hero() {
   return (
     <Container id="home">
-      <meta name="keywords" content="Kadari Deva,Web Developer,Portfolio,Python,JavaScript,HTML,CSS,React" />
+      <meta
+        name="keywords"
+        content="Kadari Deva, Web Developer, Portfolio, Python, JavaScript, HTML, CSS, React"
+      />
+
       <div className="hero-text">
-        <ScrollAnimation animateIn="fadeInUp">
-          <p>Hello <img src={Hello} alt="Hello" width="20px" />, I'm</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
-          <h1>Kadari Deva</h1>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
-          <h3>Web Developer</h3>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">Fresher</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
-          <BrowserRouter>
-            <NavHashLink smooth to="#contact" className="button">Contact</NavHashLink>
-          </BrowserRouter>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
-          <div className="social-media"><a
-            href="https://www.linkedin.com/in/deva-kadari-2399781a"
+        <p>
+          Hello <img src={Hello} alt="Hello" width="20px" />, I'm
+        </p>
+
+        <h1>Kadari Deva</h1>
+
+        <h3>Web Developer</h3>
+
+        <p className="small-resume">Fresher</p>
+
+        <BrowserRouter>
+          <NavHashLink smooth to="#contact" className="button">
+            Contact
+          </NavHashLink>
+        </BrowserRouter>
+
+        <div className="social-media">
+          <a
+            href="https://www.linkedin.com/"
             target="_blank"
             rel="noreferrer"
           >
             <img src={linkedin} alt="Linkedin" />
           </a>
-            <a
-              href="https://github.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={githubIcon} alt="GitHub" />
-            </a>
-            <a
-              href="https://api.whatsapp.com/send/?phone=%2B919938496327&text=Hello+Kadari+I+found+your+contact+through+portfolio+site.%0A%0A"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={whatsapp} alt="Whatsapp" />
-            </a>
-            <a
-              href="https://t.me/KadariDeva"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={telegram} alt="telegram" />
-            </a></div>
-        </ScrollAnimation>
+
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubIcon} alt="GitHub" />
+          </a>
+
+          <a
+            href="https://api.whatsapp.com/send/?phone=%2B919938496327"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={whatsapp} alt="Whatsapp" />
+          </a>
+
+          <a
+            href="https://t.me/KadariDeva"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={telegram} alt="Telegram" />
+          </a>
+        </div>
       </div>
+
       <div className="hero-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={1 * 1000}>
-          <img src={Illustration} alt="Ilustração" />
-        </ScrollAnimation>
+        <img src={Illustration} alt="Illustration" />
       </div>
     </Container>
-  )
+  );
 }
-
-
